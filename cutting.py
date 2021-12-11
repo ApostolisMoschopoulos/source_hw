97314b473bfe844b996c18717602e8d32ed8a89c
HW1_Big_Data <apotremosko09@gmail.com>
2021-12-11 15:01:03 +0200

    500 chunk is too short, the nature of the problem cannot be demonstrated

--- cutting.py
+++ cutting.py
@@ -5,10 +5,12 @@ data= pandas.read_csv(file_name)
 indiv_ip = data.ip.unique()
 print('Unique ip adresses: '+str(len(indiv_ip)))
 #the dataset is sorted by dates (technically by time but we aren't interested in that column)
-#We can sort for ip 
+#We can sort for ip
 clean_data = data.filter(['ip','date','extention'], axis=1)
 clean_data.sort_values(by=['ip'], inplace=True)
 print("The shape of the dataset: "+str(clean_data.shape))
 #We can also go a step futher; We can delete the rows where ip, date, extention are same
 clean_data.drop_duplicates(subset=['ip','date','extention'], keep='first', inplace=True)
 print("Instances without multiple accesses in 1 day: "+str(clean_data.shape))
+clean_data.to_csv("clean_data_500.csv",index=False)
+print("Clean data csv created")
