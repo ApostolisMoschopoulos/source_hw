192081d697f84bf0284d38b839037ec989236b2d
HW1_Big_Data <apotremosko09@gmail.com>
2021-12-11 14:05:06 +0200

    reducing the size of the dataset- that should happen at the beggining

---
+++ cutting.py
@@ -0,0 +1,14 @@
+import pandas
+file_name = 'sample_500_with_fixed_extention.csv'
+data= pandas.read_csv(file_name)
+
+indiv_ip = data.ip.unique()
+print('Unique ip adresses: '+str(len(indiv_ip)))
+#the dataset is sorted by dates (technically by time but we aren't interested in that column)
+#We can sort for ip 
+clean_data = data.filter(['ip','date','extention'], axis=1)
+clean_data.sort_values(by=['ip'], inplace=True)
+print("The shape of the dataset: "+str(clean_data.shape))
+#We can also go a step futher; We can delete the rows where ip, date, extention are same
+clean_data.drop_duplicates(subset=['ip','date','extention'], keep='first', inplace=True)
+print("Instances without multiple accesses in 1 day: "+str(clean_data.shape))
